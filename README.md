9d897d2808eb59e458b175e6e5fa81524c7bc856
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2023-06-01 15:24:41 +0200

    Initial commit with dash-cytoscape network visualization

---
+++ README.md
@@ -0,0 +1,23 @@
+# Franz Moufang - The Social Network
+
+This is a network visualization based on `dash-cytoscape` for the art exhibition [THE SOCIAL NETWORK. Die Gästebücher des Kunstsammlers Franz Moufang](https://www.uni-heidelberg.de/de/veranstaltungen/the-social-network-die-gaestebuecher-des-kunstsammlers-franz-moufang-2023-06-09) in the Kunstverein Heidelberg.
+
+## How to install and run
+
+The web app needs to be served locally or over the internet.
+
+Requirements:
+* Python + Pip
+
+Instructions:
+
+```bash
+python -m pip install -r requirements.txt
+python app.py
+```
+
+In case you are serving non-locally, you should use a proper webserver.
+
+## License
+
+The code in `app.py` is licensed under the MIT license. The data in `data.json` is licensed t.b.d.
---
+++ app.py
@@ -0,0 +1,201 @@
+from dash import Dash, html, Output, Input, State
+import dash_cytoscape as cyto
+import flask
+import json
+
+# Instantiate the Dash app
+server = flask.Flask(__name__)
+app = Dash(__name__, server=server)
+
+# Load the data file
+with open("data.json") as f:
+    data = json.load(f)
+
+# Process the data to fit our needs
+nodes = []
+edges = []
+
+for node in data["nodes"]:
+    neighbors = []
+    for edge in data["edges"]:
+        if node["id"] == edge["source"]:
+            neighbors.append(edge["target"])
+        if node["id"] == edge["target"]:
+            neighbors.append(edge["source"])
+
+    newnode = {
+        "data": {
+            "id": node["id"],
+            "label": node["label"],
+            "color": node["color"],
+            "size": str(float(node["size"]) * 10),
+            "neighbors": neighbors,
+        },
+        "position": {"x": node["x"], "y": node["y"]},
+        "grabbable": False,
+    }
+
+    if "kurzinformation" in node["attributes"]:
+        newnode["data"]["text"] = node["attributes"]["kurzinformation"]
+
+    if "lebensdaten" in node["attributes"]:
+        newnode["data"]["lebensdaten"] = node["attributes"]["lebensdaten"]
+
+    nodes.append(newnode)
+
+for edge in data["edges"]:
+    edgedata = {"data": {}}
+    edgedata["data"]["source"] = edge["source"]
+    edgedata["data"]["target"] = edge["target"]
+    edgedata["data"]["id"] = edge["id"]
+
+    node_colors = []
+    for node in data["nodes"]:
+        if node["id"] in [edge["source"], edge["target"]]:
+            node_colors.append(node["color"])
+
+    assert len(node_colors) == 2
+    if node_colors[0] == node_colors[1]:
+        edgedata["data"]["color"] = node_colors[0]
+    else:
+        edgedata["data"]["color"] = "#666666"
+
+    edges.append(edgedata)
+
+
+# The sidebar CSS
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "right": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# The sidebar object
+sidebar = html.Div(
+    [
+        html.Button("Zurück zum Netzwerk", id="return-to-full"),
+        html.Hr(),
+        html.H2(
+            "Franz Moufang: The Social Network",
+            className="display-4",
+            id="sidebar-title",
+        ),
+        html.Hr(),
+        html.P(
+            [
+                html.P(
+                    "Der Graph zeigt das private und berufliche Netzwerk von Franz Moufang, das anhand seiner Gästebücher und Briefe rekonstruiert werden konnte."
+                ),
+                html.P(
+                    "Die Farbverteilung repräsentiert annähernd die verschiedenen Interessen- und Freundesgruppen, die sich im Netzwerk von Franz Moufang entwickelt haben."
+                ),
+            ],
+            className="lead",
+            id="sidebar-content",
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+    id="info-panel",
+)
+
+# The network object
+network = html.Div(
+    [
+        cyto.Cytoscape(
+            id="cytoscape-network",
+            layout={"name": "preset"},
+            minZoom=0.05,
+            maxZoom=3,
+            style={
+                "width": "100%",
+                "height": "1000px",
+                "background-image": "url(/assets/moufang.png)",
+                "background-opacity": 0.1,
+                "background-size": "contain",
+            },
+            elements=nodes + edges,
+            stylesheet=[
+                {
+                    "selector": "node",
+                    "style": {
+                        "label": "data(label)",
+                        "background-color": "data(color)",
+                        "width": "data(size)",
+                        "height": "data(size)",
+                        "font-size": "150px",
+                    },
+                },
+                {
+                    "selector": "edge",
+                    "style": {
+                        "line-color": "data(color)",
+                        "width": "8px",
+                    },
+                },
+            ],
+        )
+    ]
+)
+
+
+@app.callback(
+    Output("sidebar-title", "children", allow_duplicate=True),
+    Output("sidebar-content", "children", allow_duplicate=True),
+    Output("cytoscape-network", "elements", allow_duplicate=True),
+    Input("cytoscape-network", "tapNodeData"),
+    prevent_initial_call=True,
+)
+def displayNodeData(data):
+    if data:
+        filtered_nodes = []
+        for node in nodes:
+            if node["data"]["id"] in data["neighbors"] + [data["id"]]:
+                filtered_nodes.append(node)
+
+        filtered_edges = []
+        for edge in edges:
+            if edge["data"]["source"] in data["neighbors"] + [data["id"]] and edge[
+                "data"
+            ]["target"] in data["neighbors"] + [data["id"]]:
+                filtered_edges.append(edge)
+
+        content = []
+
+        if "text" in data:
+            content.append(html.P([html.B("Kurzinformation: "), data["text"]]))
+
+        if "lebensdaten" in data:
+            content.append(html.P([html.B("Lebensdaten: "), data["lebensdaten"]]))
+
+        connections = [html.B("Verbindungen: "), html.Br()]
+        for neighbor in filtered_nodes:
+            if neighbor["data"]["id"] != data["id"]:
+                connections.append(neighbor["data"]["label"])
+                connections.append(html.Br())
+
+        content.extend(connections)
+
+        return data["label"], content, filtered_edges + filtered_nodes
+
+    return None, None, nodes + edges
+
+
+@app.callback(
+    Output("info-panel", "children", allow_duplicate=True),
+    Output("cytoscape-network", "elements", allow_duplicate=True),
+    Input("return-to-full", "n_clicks"),
+    prevent_initial_call=True,
+)
+def return_to_full(n_clicks):
+    return sidebar, nodes + edges
+
+
+app.layout = html.Div([network, sidebar])
+
+
+if __name__ == "__main__":
+    app.run_server(debug=False)
